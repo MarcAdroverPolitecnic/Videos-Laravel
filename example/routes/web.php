69f9b0aa4c657266e5a8ccc953ef8291059ab824
MarcAdroverPolitecnic <148445126+MarcAdroverPolitecnic@users.noreply.github.com>
2026-01-09 09:28:52 +0100

    Make a Login and Registration Part1

--- example/routes/web.php
+++ example/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -48,3 +50,11 @@ Route::resource('jobs', JobController::class);
 //});
 
 Route::view('/contact', 'contact');
+
+
+
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::get('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
