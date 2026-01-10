c5b2dc87bd23e0c7e479d60029f6b7c168c44066
MarcAdroverPolitecnic <148445126+MarcAdroverPolitecnic@users.noreply.github.com>
2026-01-10 16:20:57 +0100

    Blade and Tailwind Techniques

--- pixel-positions/resources/views/components/layout.blade.php
+++ pixel-positions/resources/views/components/layout.blade.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Pixel Positions</title>
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;500;600;&display=swap">
     @vite(['resources/js/app.js', 'resources/css/app.css'])
 </head>
-<body class="bg-black text-white">
+<body class="bg-black text-white font-hanken-grotesk">
 
     <div class="px-10">
         <nav class="flex justify-between items-center py-4 border-b border-white/10 ">
